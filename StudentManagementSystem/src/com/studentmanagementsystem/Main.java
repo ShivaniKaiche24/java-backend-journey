240295a6fb7f16f9917a91c3823552d080bea148
Shivanikaiche07 <shivanikaiche7@gmail.com>
2026-05-24 20:01:16 +0530

    Completed Day 2 student management system with add and display functionality

--- StudentManagementSystem/src/com/studentmanagementsystem/Main.java
+++ StudentManagementSystem/src/com/studentmanagementsystem/Main.java
@@ -7,26 +7,50 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		while(true) {
+		Student[] students = new Student[5];
+		int count =0;
+		
+		boolean running = true;
+		
+		while(running) {
 			System.out.println("1. Add Student");
-			System.out.println("1. Display Student");
-			System.out.println("1. Exit");
+			System.out.println("2. Display Student");
+			System.out.println("3. Exit");
 			
 			int choice = sc.nextInt();
 			
 			switch(choice) {
 			  
 			case 1:
-				System.out.println("Add Student Selected");
+				System.out.println("Enter Student ID: ");
+				int id = sc.nextInt();
+				
+				System.out.println("Enter Student Name: ");
+				String name = sc.next();
+				
+				System.out.println("Enter Student Marks: ");
+				double marks= sc.nextDouble();
+				
+				Student student = new Student(id,name,marks);
+				
+				students[count] = student;
+				count++;
+				
+				System.out.println("Student Added Successfully");
+				
 				break;
 			
 			case 2:
-				System.out.println("Display Student Selected");
+				for(int i =0;i<count;i++) {
+					students[i].displayStudent();
+				}
 				break;
 			
 			case 3:
 				System.out.println("Exiting...");
+				running=false;
 				break;
+			
 				
 			default:
 				System.out.println("Invalid Choice");
