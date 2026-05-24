bf6605d6b65a81e974586e9d0aa29c5a0547781e
Shivanikaiche07 <shivanikaiche7@gmail.com>
2026-05-24 19:11:47 +0530

    Created Student Class and constructor for Student Management System

--- StudentManagementSystem/src/com/studentmanagementsystem/Student.java
+++ StudentManagementSystem/src/com/studentmanagementsystem/Student.java
@@ -2,4 +2,20 @@ package com.studentmanagementsystem;
 
 public class Student {
 
+	int id;            // instance variables / fields
+	String name;
+	double marks;
+	
+	
+	public Student (int id , String name , double marks) { //Constructors are special methods automatically called during object creation
+		this.id=id; //this.id--> current object  id --> Constructor parameter and this value would be given by user when the object is created 
+		this.name=name;
+		this.marks=marks;
+	}
+	
+	public void displayStudent() {
+		System.out.println("ID: " + id);
+		System.out.println("Name: " + name);
+		System.out.println("Marks: " + marks);
+	}
 }
